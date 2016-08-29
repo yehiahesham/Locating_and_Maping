a1747cceea809c2f0d4aa1e95c4682f2b623159e
yehiahesham <yehia.hisham2012@gmail.com>
2016-08-29 11:56:10 +0200

    industrial grade

--- testing_maps.py
+++ testing_maps.py
@@ -2,34 +2,25 @@ import googlemaps
 import json
 from pprint import pprint
 import sys
+from datetime import datetime
+import time
+
 
 mykey=''
 gmaps = googlemaps.Client(key=mykey)
 
-def yehia ():
-    print 'hello'
 
-
-def distance_matrix(source,distination):
+def distance_matrix(source,distination,mode=None,departure_time=None,arrival_time=None): #returns distance and time (default mode is driving)
+#departure_time/arrival_time =>int or datetime.datetime,
     global gmaps
-    my_distance=  gmaps.distance_matrix(distination,source)
+    my_distance=  gmaps.distance_matrix(distination,source,mode,departure_time,arrival_time)
     return my_distance['rows'][0]['elements'][0]['distance']['text'],my_distance['rows'][0]['elements'][0]['duration']['text']
 
 
 def getadress(Latlng): #returns address
     global gmaps
     my_address=gmaps.reverse_geocode(Latlng)
-    print my_address
-    for data in my_address:
-        print "\n ---------- address ---------- "
-        for address in data['address_components']:
-             sys.stdout.write(address['short_name']+' ')
-    print "\n"
-
-def PlaceDetails(Latlng):
-    global gmaps
-    my_place=gmaps.places(Latlng)
-    print my_place
+    return my_address[0]['formatted_address']
 
 
 def getLatlng(addr): #returns Latlng
@@ -37,17 +28,6 @@ def getLatlng(addr): #returns Latlng
     my_longlat= gmaps.geocode(addr)
     return  my_longlat[0]['geometry']['location']['lat'],my_longlat[0]['geometry']['location']['lng']
 
-
-if __name__ == "__main__":
-    source='30.019200, 31.502364'
-    distination='29.988083, 31.441536'
-
-#    dis,dur=distance_matrix(source,distination)
-    lat,lng =getLatlng('Facebook HQ, Hacker Way, Menlo Park, CA, United States')
-    x=str(lat)+','+str(lng)
-    getadress(x)
-    PlaceDetails(x)
-
-#    print ("Distance is "+ dis)
-#    print ("duration is "+ dur)
-    print "Latlng is : %f ,%f " % (lat, lng)
+print distance_matrix('37.419356, -122.093706','37.421899, -122.084147',mode="bicycling",departure_time=datetime.now())
+print getadress('37.419356, -122.093706')
+print getLatlng(getadress('37.419356, -122.093706'))
