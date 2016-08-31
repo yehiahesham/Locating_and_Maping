6ef46547d59c070859b289b57fa9bc934fe98a0b
yehiahesham <yehia.hisham2012@gmail.com>
2016-08-31 17:07:33 +0200

    receving results from Commands to Tile38 Server using Python

--- update_tile38DB.py
+++ update_tile38DB.py
@@ -1,6 +1,17 @@
 import requests
 
 def Tile38_Command(command, IP=None , port=None ):
+	'''
+	This function acts like the tile38-cli executable binary 
+	:param command: is a command that will be passed to the Tile38 server who will executed it , Check commands at: tile38.com/commands/
+	:param type command is string
+
+	:param IP: IP of the Tile38 , default here is localhost which is 127.0.0.1
+	:param type IP is string
+
+	:param port: port on which Tile38 is listening to ,default here is port 9851
+	:param type port is unsigned int
+	'''
 	data = command
 	if not IP:
 		print "Default IP is assumed = localhost"
@@ -11,11 +22,13 @@ def Tile38_Command(command, IP=None , port=None ):
 		print "Default Port is assumed = 9851"
 		port = 9851
 	url= str(IP)+":"+str(port)
-	requests.post(url, data=data)
+	response = requests.post(url, data=data)
+	return response.content	
+
 
 #usage example
-Tile38_Command('set fleet abc1 point 12.4762 -111.10923')
+print Tile38_Command('get fleet abc1')
 print '\n'
-Tile38_Command('set fleet abc2 point 13.4762 -111.10923','127.0.0.1')
+print Tile38_Command('set fleet abc12 point 13.4762 -111.10923','127.0.0.1')
 print '\n'
-Tile38_Command('set fleet abc3 point 14.4762 -111.10923','127.0.0.1',9851)
+print Tile38_Command('get fleet fb','127.0.0.1',9851)
