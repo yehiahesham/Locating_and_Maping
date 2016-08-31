d9264129eb08f5def2a0b29c2dff688b7363ed74
yehiahesham <yehia.hisham2012@gmail.com>
2016-08-31 10:49:21 +0200

    made a class out of it

--- testing_maps.py
+++ testing_maps.py
@@ -5,28 +5,33 @@ from datetime import datetime
 import time
 
 
-mykey=''
-gmaps = googlemaps.Client(key=mykey)
+
+class Py_GOOGLE_MAPS_API:
+    """docstring for Py_GOOGLE_MAPS_API"""
+    def __init__(self, _mykey):
+        self.mykey =_mykey
+        self.gmaps = googlemaps.Client(key=self.mykey)
+
+    def distance_matrix(self,source,distination,mode=None,departure_time=None,arrival_time=None):
+        my_distance=  self.gmaps.distance_matrix(distination,source,mode,departure_time,arrival_time)
+        return my_distance['rows'][0]['elements'][0]['distance']['text'],my_distance['rows'][0]['elements'][0]['duration']['text']
 
 
-def distance_matrix(source,distination,mode=None,departure_time=None,arrival_time=None): #returns distance and time (default mode is driving)
-#departure_time/arrival_time =>int or datetime.datetime,
-    global gmaps
-    my_distance=  gmaps.distance_matrix(distination,source,mode,departure_time,arrival_time)
-    return my_distance['rows'][0]['elements'][0]['distance']['text'],my_distance['rows'][0]['elements'][0]['duration']['text']
+    def getadress(self,Latlng): #returns address
+        my_address=self.gmaps.reverse_geocode(Latlng)
+        return my_address[0]['formatted_address']
 
 
-def getadress(Latlng): #returns address
-    global gmaps
-    my_address=gmaps.reverse_geocode(Latlng)
-    return my_address[0]['formatted_address']
+    def getLatlng(self,addr): #returns Latlng
+        my_longlat= self.gmaps.geocode(addr)
+        return  my_longlat[0]['geometry']['location']['lat'],my_longlat[0]['geometry']['location']['lng']
+
+    def returnMe(self):
+        return self
 
 
-def getLatlng(addr): #returns Latlng
-    global gmaps
-    my_longlat= gmaps.geocode(addr)
-    return  my_longlat[0]['geometry']['location']['lat'],my_longlat[0]['geometry']['location']['lng']
-
-print distance_matrix('37.419356, -122.093706','37.421899, -122.084147',mode="bicycling",departure_time=datetime.now())
-print getadress('37.419356, -122.093706')
-print getLatlng(getadress('37.419356, -122.093706'))
+p= Py_GOOGLE_MAPS_API('PUT_YOUR_KEY_HERE')
+print p.distance_matrix('37.419356, -122.093706','37.421899, -122.084147',mode="bicycling",departure_time=datetime.now())
+print p.getadress('37.419356, -122.093706')
+print p.getLatlng(p.getadress('37.419356, -122.093706'))
+print p.returnMe()
