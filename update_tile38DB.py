5c909a6adfb2e1d01dbe1cc8bbeb131ac6dd7b1b
yehiahesham <yehia.hisham2012@gmail.com>
2016-08-31 18:08:05 +0200

    made Tile38 a class and created a generall DB Class

--- update_tile38DB.py
+++ update_tile38DB.py
@@ -1,34 +1,73 @@
 import requests
+import MySQLdb
 
-def Tile38_Command(command, IP=None , port=None ):
-	'''
-	This function acts like the tile38-cli executable binary 
-	:param command: is a command that will be passed to the Tile38 server who will executed it , Check commands at: tile38.com/commands/
-	:param type command is string
+class Tile38:
+	#This function wrapps  tile38-cli executable
+	def __init__(self,IP=None , port=None ):
+		'''
+			:param IP: IP of the Tile38 , default here is localhost which is 127.0.0.1
+			:param type IP is string
 
-	:param IP: IP of the Tile38 , default here is localhost which is 127.0.0.1
-	:param type IP is string
+			:param port: port on which Tile38 is listening to ,default here is port 9851
+			:param type port is unsigned int
+		'''
+		if not IP:
+			print "Default IP is assumed = localhost"
+			self.IP='http://localhost'
+		else:
+		 self.IP='http://'+IP
+		if not port:
+			print "Default Port is assumed = 9851"
+			self.port = 9851
+		else:
+			self.port = port
+		self.url= str( self.IP)+":"+str(self.port)
 
-	:param port: port on which Tile38 is listening to ,default here is port 9851
-	:param type port is unsigned int
-	'''
-	data = command
-	if not IP:
-		print "Default IP is assumed = localhost"
-		IP='http://localhost'
-	else:
-	 IP='http://'+IP
-	if not port:
-		print "Default Port is assumed = 9851"
-		port = 9851
-	url= str(IP)+":"+str(port)
-	response = requests.post(url, data=data)
-	return response.content	
+	def Command(self,command, IP=None , port=None ):
+		'''
+		This function  acts like the tile38-cli sending commands to the  server
+			:param command: is a command that will be passed to the Tile38 server who will executed it , Check commands at: tile38.com/commands/
+			:param type command is string
+		'''
+		response = requests.post(self.url, data=command)
+		return response.content
+
+
+
+
+
+class DataBase:
+	def __init__(self,connection,User,Password,Database):
+		self.connection=connection
+		self.User=User
+		self.Password=Password
+		self.Database=Database
+		self.db = MySQLdb.connect(connection,User,Password,Database)
+
+	def Insert(self,SQLStatment):
+		cursor = self.db.cursor()
+		try:
+			cursor.execute(SQLStatment)
+			db.commit()
+		except:
+			db.rollback()
+		self.db.close()
+
+	def Select(self,SQLStatment):
+		cursor = self.db.cursor()
+		cursor.execute(SQLStatment)
+		results = cursor.fetchall()
+		self.db.close()
+		return results
 
 
 #usage example
-print Tile38_Command('get fleet abc1')
-print '\n'
-print Tile38_Command('set fleet abc12 point 13.4762 -111.10923','127.0.0.1')
-print '\n'
-print Tile38_Command('get fleet fb','127.0.0.1',9851)
+
+t= Tile38('127.0.0.1',9851)
+db= DataBase('localhost','user','','BioMedical_DB')
+c =  db.Select("SELECT ID, Name, Acc_Type, latitude, longitude FROM Accounts WHERE ID=2")
+for row in c:
+	name = row[1]
+	lat=row[3]
+	lng=row[4]
+	print t.Command('set fleet '+str(name.replace(" ", "_"))+' point '+str(lat)+' '+str(lng))
